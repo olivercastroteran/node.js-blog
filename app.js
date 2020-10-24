7446eedb0e48110630904dcc34cf79f09377226e
oliver <olivercastroteran@gmail.com>
2020-10-23 23:44:44 -0400

    app connected to mongoDB

--- app.js
+++ app.js
@@ -1,40 +1,50 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+const dbURI = require('./db');
 
 // setup express app
 const app = express();
 
+// connect to mongodb
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log('Connected to DB');
+    // listen for requests
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
-
 // middleware & static files
 app.use(express.static('public'));
-
 app.use(morgan('dev'));
 
+// routes
 app.get('/', (req, res) => {
-  const blogs = [
-    {
-      title: 'Yoshi finds eggs',
-      snippet: 'Lorem ipsum dolor sit amet consectetur',
-    },
-    {
-      title: 'Mario finds stars',
-      snippet: 'Lorem ipsum dolor sit amet consectetur',
-    },
-    {
-      title: 'How to defeat bowser',
-      snippet: 'Lorem ipsum dolor sit amet consectetur',
-    },
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'Blogs', blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get('/blogs/create', (req, res) => {
