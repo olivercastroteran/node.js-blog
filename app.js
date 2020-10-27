da7f5506b41725c931cfddbb720c064aade9845e
oliver <olivercastroteran@gmail.com>
2020-10-26 23:02:27 -0400

    CRUD blogs operations implemented

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
 const dbURI = require('./db');
+const { render } = require('ejs');
 
 // setup express app
 const app = express();
@@ -24,6 +25,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // routes
@@ -45,6 +47,35 @@ app.get('/blogs', (req, res) => {
     .catch((err) => {
       console.log(err);
     });
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch((err) => console.log(err));
 });
 
 app.get('/blogs/create', (req, res) => {
